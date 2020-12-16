95370bfa83692c6b1b3166fab30fe3f72f7c8651
Gabriel Torrandella <torrandellag@gmail.com>
2020-12-16 11:37:39 -0300

    Added GET activity to api

--- api/server.py
+++ api/server.py
@@ -9,17 +9,34 @@ app = Flask(__name__)
 CORS(app)
 db_connector = Connector()
 
+@app.route('/actividad/<int:id>', methods = ['GET'])
+def actividad_by_id(id):
+    try:
+        return db_connector.getActivityByKey(id)
+    except:
+        return Response(400)
+
 @app.route('/actividad', methods = ['GET'])
-def api_logger():
-    if 'Content-Type' in request.headers.keys():
-        return Response(status = 200)                
-    else:
-        return Response(status = 400)
+def actividad_by_multimedia_id():
+    try:
+        multimedia = request.args.get('multimedia')
+        return db_connector.getActivityByMultimediaKey(multimedia)
+    except:
+        return Response(400)
+
 
 @app.route('/multimedia/<int:id>', methods = ['GET'])
 def multimedia_by_id(id):
     try:
         return db_connector.getMultimediaByKey(id)
+    except:
+        return Response(400)
+
+@app.route('/multimedia', methods = ['GET'])
+def multimedia_by_actividad_id():
+    try:
+        actividad = request.args.get('actividad')
+        return db_connector.getMultimediaByActivityKey(actividad)
     except:
         return Response(400)
 
